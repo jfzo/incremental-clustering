c9e79b1baa171236a3ce565aa66f4f8c21efe843
chris <christ.ale.nu.so13@gmail.com>
2020-11-12 00:42:26 -0300

    se realizaron los cambios del paso de data frame a solo arrays. se elimino la parte que estaba extra del codigo con estos cambios(lo cual seria borrar desde la linea 5 a la 37). se utilzan tokens en lugar de shingles. se adapta el resto del codigo para la utilización de los nuevos datos. utilizacion de los textos incluidos en sklearn.

--- lsh snn.py
+++ lsh snn.py
@@ -1,85 +1,57 @@
 import numpy as np
 import pandas as pd
-import random
-####
-x=["PEDRO JUGABA A LA PELOTA"]
-y=["LA PELOTA NO ERA DE JUAN"]
-z=["LA MANZANA NO ERA DE FRANCISCO"]
-def shingle(x,k):
-    x[0]=x[0].replace(" ","")
-    new_dic=[]
-    j=0
-    while(j<=len(x[0])-k):
-        new_dic+=[x[0][j:j+k]]
-        j+=1
-    return new_dic
-a,b,c=shingle(x,2),shingle(y,2),shingle(z,2)
-nlist=[a,b,c]
-nlist
-def id(x):
-    unique=[]
-    for i in range(0,len(x)):
-        for e in (x[i]):
-            if e not in unique:
-                unique.append(e)
-    return (unique)
-id(nlist)
-len(id(nlist))
-def createMatrix(x):
-    item=id(x)
-    z=np.zeros((len(item),len(x)))
-    for i in range(0,len(x)):
-        for e in range(0,len(x[i])):
-            if x[i][e] in item:
-                ind=item.index(x[i][e])
-                z[ind][i]=1
-    return z
-####cambiar lista a dict
-adjm=createMatrix(nlist)
-adjm
-np.dot(adjm.transpose(),adjm)
-df=pd.DataFrame(createMatrix(nlist))
-df.index=id(nlist)
-df.insert(0,"id",list(range(0,len(id(nlist)))))
-df
-####cambiar data frame a matriz
-#####
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import HashingVectorizer
+from sklearn.datasets import fetch_20newsgroups
+from sklearn import random_projection
+newsgroups_train = fetch_20newsgroups(subset='train',
+                                     remove=('headers', 'footers', 'quotes'),
+                                     categories=['sci.space'])
+
+
+def get_Tokens(x):
+    vectorizer = CountVectorizer(analyzer="word",ngram_range=(1,1))
+    return (vectorizer.fit_transform(x).toarray(),vectorizer.get_feature_names())
+m,names=get_Tokens(newsgroups_train.data[:20])
+len(names)
+m=np.array(m>=1,dtype=int)
+np.dot(m.transpose(),m)
 def Jaccard(X,Y):
     a=0
-    b=0
     for i in range(0,len(X)):
         if X[i]==Y[i] and X[i]==1:
             a+=1
-            b+=1
-        elif X[i]!=Y[i] and(X[i]==1 or Y[i]==1):
-            b+=1
-        else:
-            a=a
-            b=b
-    return (a/b)
-Jaccard(df[0],df[1])
-def hashfunct(x):
-    a=(x+1)/5
-    b=(3*x+1)/5
-    return (a,b)
+    return (a/len(X))
+Jaccard(m[0],m[10])
+def Extract(lst,a):
+    return [item[a] for item in lst]
 def signature(X,n,seed=8):
     np.random.seed(seed)
     k=1
     p=[]
     while(k<=n):
-        r=np.random.permutation(range(0,len(X)))
-        for e in range(0, X.shape[1]-1):
+        r=np.random.permutation(range(0,X.shape[1]))
+        for e in range(0, X.shape[0]):
             for i in r:
                 if X[e][i]==1:
                     p+=[i]
                     break
         k+=1
-    return np.split(np.asarray(p),n)
-s=signature(df,3)
-s
-s[0]
-s[1]
-s[2]
-
-Jaccard(df[0],df[1])
-Jaccard(df[1],df[2])
+    z=(np.split(np.asarray(p),n))
+    c = np.zeros((len(z[0]), len(z)), dtype=np.int32)
+    for i in range(0, len(z[0])):
+        c[i] = Extract(z, i)
+    return c
+def sim(x,y):
+    a=0
+    for i in range(0,len(x)):
+        if x[i]==y[i]:
+            a+=1
+    return a/len(x)
+s=signature(m,10000)
+s.shape
+def firma(x):
+    transformer = random_projection.SparseRandomProjection()
+    return (transformer.fit_transform(x))
+s_new=firma(s)
+s_new.shape
